57bf0650cc199d4c695ad809d1e2aa7796e674dc
Alvin Alic <alvin.alic@gmail.com>
2024-09-30 22:01:58 -0500

    I first turned the string into an array so that ea ch word is an array element. I looped through the dictionary and then looped through the string array to compare both words to see if it contained the substring from the dictionary. I checked for the substring using string[substring]. If that didn't return nil (meaning the string contained that substring from the dictionary, I would push it into my hash, creating anew key value pair.

--- sub_string.rb
+++ sub_string.rb
@@ -1,26 +1,43 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-  char_array = string.split("")
-  start = 0
-  last = string.length
   result = Hash.new
-  char_array.each do |char|
-  current_word = char_array[start..last].join("")
-  start +=1
-  
-  if(dictionary.include?(current_word))
-    if(result.key?(current_word))
-      result[current_word] += 1
-    else
-    result[current_word] = 1
-    end
-  end
-  
-  end
+  string_array = string.split" "
 
-  dictionary.include?(string)
+  dictionary.each do |dic_word|
+    string_array.each do |string_word|
+      string_word = string_word.downcase
+      if string_word[dic_word] != nil
+        if result.key?(dic_word)
+          result[dic_word] += 1
+        else
+          result[dic_word] = 1
+        end
+      end
+      end
+  end
   result
+  
+  # char_array = string.split("")
+  # start = 0
+  # last = string.length
+  # result = Hash.new
+  # char_array.each do |char|
+  # current_word = char_array[start..last].join("")
+  # start +=1
+  
+  # if(dictionary.include?(current_word))
+  #   if(result.key?(current_word))
+  #     result[current_word] += 1
+  #   else
+  #   result[current_word] = 1
+  #   end
+  # end
+  
+  # end
+
+  # dictionary.include?(string)
+  # result
 end
 
-puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
