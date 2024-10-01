b77dc2512bae5112b08bab55f18476dfb7554fc6
Alvin Alic <alvin.alic@gmail.com>
2024-09-30 22:03:56 -0500

    Remove unused code

--- sub_string.rb
+++ sub_string.rb
@@ -18,26 +18,6 @@ def substrings(string, dictionary)
   end
   result
   
-  # char_array = string.split("")
-  # start = 0
-  # last = string.length
-  # result = Hash.new
-  # char_array.each do |char|
-  # current_word = char_array[start..last].join("")
-  # start +=1
-  
-  # if(dictionary.include?(current_word))
-  #   if(result.key?(current_word))
-  #     result[current_word] += 1
-  #   else
-  #   result[current_word] = 1
-  #   end
-  # end
-  
-  # end
-
-  # dictionary.include?(string)
-  # result
 end
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
